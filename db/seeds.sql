e9e2e4064f89974aa134064d86bb500f4f337c3c
unknown <fsadler9@gmail.com>
2019-07-28 21:26:24 -0600

    fully functional

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Veggie BUrger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devour) VALUES ('Southwest', true);
+
